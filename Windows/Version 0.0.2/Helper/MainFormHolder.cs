24fbabd52d79062f36ef7735cada762ea3a93fef
Chrono <chrono_legionnaire@protonmail.com>
2021-09-09 14:30:40 +0800

    Add files via upload

---
+++ Windows/Version 0.0.2/Helper/MainFormHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PriSecFileStorageClient.Helper
+{
+    public static class MainFormHolder
+    {
+        public static Form myForm {get;set;}
+
+        public static Boolean OpenMainForm { get; set; }
+    }
+}
