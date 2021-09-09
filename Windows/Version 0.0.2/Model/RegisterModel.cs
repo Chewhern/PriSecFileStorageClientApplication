7c3719daaceffde2e034954a9c2fa0330ece7311
Chrono <chrono_legionnaire@protonmail.com>
2021-09-09 14:32:15 +0800

    Add files via upload

---
+++ Windows/Version 0.0.2/Model/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageClient.Model
+{
+    class RegisterModel
+    {
+        public String UserIDCount { get; set; }
+        public String UserIDChecker { get; set; }
+        public String Error { get; set; }
+    }
+}
