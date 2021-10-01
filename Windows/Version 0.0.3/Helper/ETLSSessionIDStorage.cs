763219d4e9f5c878ff301d854cb01ee20adaac97
Chrono <chrono_legionnaire@protonmail.com>
2021-10-01 22:29:51 +0800

    Add files via upload

---
+++ Windows/Version 0.0.3/Helper/ETLSSessionIDStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageClient.Helper
+{
+    public static class ETLSSessionIDStorage
+    {
+        public static String ETLSID { get; set; }
+    }
+}
