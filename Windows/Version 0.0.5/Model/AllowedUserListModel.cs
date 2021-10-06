843d0067ca5e6ecbef280341796a59266755a535
Chrono <chrono_legionnaire@protonmail.com>
2021-10-06 17:43:20 +0800

    Add files via upload

---
+++ Windows/Version 0.0.5/Model/AllowedUserListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageClient.Model
+{
+    public class AllowedUserListModel
+    {
+        public String Status { get; set; }
+
+        public String[] AllowedUserID { get; set; }
+    }
+}
