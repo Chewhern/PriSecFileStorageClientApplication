88bc8521dc5fd89bbfc3c7cba3013d49b2616add
Chrono <chrono_legionnaire@protonmail.com>
2022-04-04 14:00:27 +0000

    Update APIIPAddressHelper.cs

--- Cross Desktop OS/Web/V1/Helper/APIIPAddressHelper.cs
+++ Cross Desktop OS/Web/V1/Helper/APIIPAddressHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PriSecFileStorageClient.Helper
+namespace PriSecFileStorageWeb.Helper
 {
     public static class APIIPAddressHelper
     {
